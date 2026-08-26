d3d0574c89926dccfedd961f99342234de412f6f
Gokula-krishnan08 <gokula.official@gmail.com>
2026-08-26 22:22:07 +0530

    Image mapped correctly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useLocation, NavLink } from "react-router-dom";
-import logo from "../assets/Bs-logo-navbar.png";
+import logo from "../assets/BS-logo-navbar.png";
 
 const links = [
   { to: "/", label: "Home" },
